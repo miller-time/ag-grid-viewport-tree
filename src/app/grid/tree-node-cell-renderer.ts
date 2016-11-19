e844834401277f692115f44845d419cac691e505
Russell Cousineau <rcousineau@comscore.com>
2016-11-19 09:41:19 -0800

    display expand/collapse toggle button

--- src/app/grid/tree-node-cell-renderer.ts
+++ src/app/grid/tree-node-cell-renderer.ts
@@ -11,7 +11,10 @@ interface CellRendererParams {
 export class TreeNodeCellRenderer implements ICellRenderer {
   private params: CellRendererParams;
   private element: HTMLElement;
-  private valueElement: Element;
+  private toggleButton: HTMLElement;
+  private expandedElement: HTMLElement;
+  private collapsedElement: HTMLElement;
+  private valueElement: HTMLElement;
 
   init(params: CellRendererParams) {
     this.params = params;
@@ -29,25 +32,47 @@ export class TreeNodeCellRenderer implements ICellRenderer {
 
   private createElement() {
     this.element = document.createElement('div');
-    this.element.innerHTML = '<span class="group-value"></span>';
-    this.valueElement = this.element.querySelectorAll('.group-value')[0];
+    this.element.innerHTML = `
+      <button class="expand-collapse-btn">
+        <span class="expanded fa fa-plus-square-o"></span>
+        <span class="collapsed fa fa-minus-square-o"></span>
+      </button>
+
+      <span class="group-value"></span>
+    `;
+    this.toggleButton = <HTMLElement>this.element.querySelectorAll('button')[0];
+    this.expandedElement = <HTMLElement>this.element.querySelectorAll('.expanded')[0];
+    this.collapsedElement = <HTMLElement>this.element.querySelectorAll('.collapsed')[0];
+    this.valueElement = <HTMLElement>this.element.querySelectorAll('.group-value')[0];
     this.updateValue();
   }
 
   private updateValue() {
     if (this.params.data && this.params.data.group) {
-      if (!this.params.node.expanded) {
-        setTimeout(() => {
-          console.log('expanding group ' + this.params.data.group);
-          this.params.node.expanded = true;
-          console.log('firing "rowGroupOpened" for group ' + this.params.data.group);
-          this.params.api.dispatchEvent('rowGroupOpened', {
-            group: this.params.data.group,
-            expanded: this.params.node.expanded
-          });
-        }, 3000);
+      if (typeof this.params.node.expanded === 'undefined') {
+        this.params.node.expanded = true;
       }
+      this.setVisible(this.expandedElement, this.params.node.expanded);
+      this.setVisible(this.collapsedElement, !this.params.node.expanded);
       this.valueElement.innerHTML = '<b>Group ' + this.params.data.group + '</b>';
+    } else {
+      this.setVisible(this.toggleButton, false);
     }
+  }
+
+  private setVisible(element: HTMLElement, visible: boolean) {
+    if (visible) {
+      this.showElement(element);
+    } else {
+      this.hideElement(element);
+    }
+  }
+
+  private hideElement(element: HTMLElement) {
+    element.style.display = 'none';
+  }
+
+  private showElement(element: HTMLElement) {
+    element.style.display = 'inline-block';
   }
 }
