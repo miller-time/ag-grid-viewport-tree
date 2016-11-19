5310daf0b1a7d8556fb589d3b05f058827132c94
Russell Cousineau <rcousineau@comscore.com>
2016-11-19 00:16:41 -0800

    put a grid on the page

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GridOptions } from 'ag-grid/main';
+
+import { GridService, RowData } from './grid.service';
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent implements OnInit {
+  private gridOptions: GridOptions;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private gridService: GridService
+  ) {
+    this.gridOptions = {
+      enableColResize: true,
+      columnDefs: [
+        { field: 'mean',   headerName: 'Mean',   width: 200 },
+        { field: 'median', headerName: 'Median', width: 200 },
+        { field: 'mode',   headerName: 'Mode',   width: 200 },
+        { field: 'range',  headerName: 'Range',  width: 200 },
+      ]
+    };
   }
 
+  ngOnInit() {
+    this.gridService.getData()
+      .subscribe((rowData: RowData[]) => {
+        this.gridOptions.api.setRowData(rowData);
+      });
+  }
 }
